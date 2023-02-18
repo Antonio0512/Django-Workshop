f44afff217ed4af91f1b01d53280929117c47322
Antonio <antonioboyanov9@gmail.com>
2023-02-18 15:22:33 +0200

    Update settings.py

--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -7,9 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jxjl9-x)uvndqgg2&av@zi@4!cq!+s)#-#0!&v_ub#$ee#y17#'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ["127.0.0.1"]
